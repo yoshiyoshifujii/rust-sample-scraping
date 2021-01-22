41aa7f9680f84a05d4e6764ad37614d723e85f7c
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-01-22 18:12:25 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use scraper::{Selector, Html};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let response = reqwest::blocking::get("https://blog.rust-lang.org")?;
+    let body = response.text()?;
+    let document = Html::parse_document(&body);
+
+    let selector = Selector::parse(".post-list tr > td.tr + td > a").unwrap();
+    let elements = document.select(&selector);
+
+    let f: fn(()) = |e| println!("{}", e.text().next().unwrap());
+    elements.for_each(|e| println!("{}", e.text().next().unwrap()));
+    elements.map(|e| "");
+
+    Ok(())
 }
